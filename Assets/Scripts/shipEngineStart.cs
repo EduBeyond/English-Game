ae90f6b55c4fd843523ab5634739b1e9b51785fc
Enzo Passarini <enzopassarini@Enzos-MacBook-Air.local>
2022-12-21 23:27:50 -0800

    intro done organized assets

--- Assets/Scripts/shipEngineStart.cs
+++ Assets/Scripts/shipEngineStart.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class shipEngineStart : MonoBehaviour
 {
     [SerializeField] private Animator Spaceship;
+    [SerializeField] private Animator LevelLoader;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,7 @@ public class shipEngineStart : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
         Spaceship.Play("shipHyperspace", 0, 4.0f);
+        LevelLoader.Play("outro_crossfade", 0, 4.0f);
         }
 
     }
